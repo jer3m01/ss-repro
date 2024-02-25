2e01a1d04176bfcf4e8a62c7af1a4226cbc660e6
jer3m01 <jer3m01@jer3m01.com>
2024-02-25 02:29:52 +0100

    stripping

--- apps/docs/src/routes/docs/core.tsx
+++ apps/docs/src/routes/docs/core.tsx
@@ -1,24 +1,7 @@
 import { RouteProps } from "@solidjs/router";
 
 import { Layout } from "../../components";
-import { NavSection } from "../../model/navigation";
-
-const CORE_NAV_SECTIONS: NavSection[] = [
-	{
-		title: "Overview",
-		links: [
-			{
-				title: "Introduction",
-				href: "/docs/core/overview/introduction",
-			},
-			{
-				title: "Getting started",
-				href: "/docs/core/overview/getting-started",
-			},
-		],
-	},
-];
 
 export default function CoreLayout(props: RouteProps<string>) {
-	return <Layout navSections={CORE_NAV_SECTIONS}>{props.children}</Layout>;
+	return <Layout>{props.children}</Layout>;
 }
